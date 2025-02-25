2513e361c3975680cc7d6fe96ff684ddd54726b2
Nibir2405 <nibirislam56@gmail.com>
2025-02-26 00:15:04 +0600

    Add new PDF invoice generation functionality and sample files

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
-print(filepaths)
-total = 0
 for filepath in filepaths:
-    df = pd.read_excel(filepath)
+    
     pdf = FPDF(orientation="p", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
     invoice_nr, date = filename.split("-")
 
@@ -18,7 +17,54 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice num. {invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date {date}",)
+    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1)
+
+
+    df = pd.read_excel(filepath)
+
+
+#Add Header
+ 
+    column = [item.replace("_"," ").title() for item in list(df.columns)]
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=column[0], border=1)
+    pdf.cell(w=70, h=8, txt=column[1], border=1)
+    pdf.cell(w=30, h=8, txt=column[2], border=1)
+    pdf.cell(w=30, h=8, txt=column[3], border=1)
+    pdf.cell(w=30, h=8, txt=column[4], border=1, ln=1)
+
+# Add rows to the tables
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    #Total value adding on the end of the row 
+       
+    total = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1)
+
+    #Add total price sentence
+
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The Total Price is {total} taka", ln=1)
+
+    #Add company name and logo
+
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=40, h=8, txt="SILICA SHEARD")
+    pdf.image("silica_shard.png", w=10)
 
     pdf.output(f"PDFs/{filename}")
 
